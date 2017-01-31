1c42a711ff2958584fbc7b104b5c8ca26536ea5e
JoP <jop.mezzana@gmail.com>
2017-01-31 22:03:38 +0100

    Web app PoC

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 var express = require('express')
 var app = express()
 
+app.use('/light/jopcave', function(req, res, next) {
+  console.log('Requested light ' + req.query.status + ' from ' + req.ip );
+  next();
+})
+
 app.get('/light/jopcave', function (req, res) {
-  if (req.params.status === 'on') {
-    // accippa
+  if (req.query.status === 'on') {
+    // TODO accippa
   }
-  else if (req.params.status === 'off') {
-    // spegni
+  else if (req.query.status === 'off') {
+    // TODO spegni
   }
-  res.send(200)
+  res.sendStatus(200)
 })
 
 app.get('/light/jopcave/status', function(req, res) {
+  // TODO
   res.send({
     id: 'jopcave',
     status: true
   })
 })
+
+app.use('/', express.static('static'))
 
 app.listen(8077, function () {
   console.log('Light is go on port 8077')
